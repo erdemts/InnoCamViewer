0da76f4bf2d8d5246409ab1e465900c3a86ebf23
erdemts <erdemts@users.noreply.github.com>
2018-05-26 02:31:25 +0300

    Initial commit

---
+++ OCX/register.bat
@@ -0,0 +1,3 @@
+regsvr32 C:\InnoCamViewer\OCX\VideoKernelX.ocx
+regsvr32 C:\InnoCamViewer\OCX\VideoConverterX.ocx
+regsvr32 C:\InnoCamViewer\OCX\FileClient.ocx
---
+++ OCX/unregister.bat
@@ -0,0 +1,3 @@
+regsvr32 -u C:\InnoCamViewer\OCX\VideoKernelX.ocx
+regsvr32 -u C:\InnoCamViewer\OCX\VideoConverterX.ocx
+regsvr32 -u C:\InnoCamViewer\OCX\FileClient.ocx
